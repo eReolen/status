ae8110cf38dc3129b0c4223ba03f852149b6d91e
Jesper Kristensen <cableman@linuxdev.dk>
2022-02-14 13:58:40 +0100

    ER-1174: Added first datewell search command

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     ItkDev\MetricsBundle\ItkDevMetricsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
---
+++ src/Command/DatawellTimeCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\DataWell\SearchService;
+use ItkDev\MetricsBundle\Service\MetricsService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+class DatawellTimeCommand extends Command
+{
+    protected static $defaultName = 'app:datawell:stats';
+    protected static $defaultDescription = 'Get time for data well connection';
+
+    private SearchService $searchService;
+    private MetricsService $metricsService;
+
+    public function __construct(SearchService $searchService, MetricsService $metricsService)
+    {
+        parent::__construct();
+        $this->searchService = $searchService;
+        $this->metricsService = $metricsService;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $stopwatch = new Stopwatch(true);
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $stopwatch->start('request');
+        $result = $this->searchService->search('facet.type=ebog');
+        $event = $stopwatch->stop('request');
+        $seconds = $event->getDuration() / 1000;
+
+        $this->metricsService->histogram('datawell_search_duration_seconds', '', $seconds);
+        $this->metricsService->histogram('datawell_reported_duration_seconds', '', $result[3]);
+
+        if ($output->isVerbose()) {
+            $io->success('Request time: ' . $seconds . ', Reported time: ' . $result[3]);
+        }
+
+        return Command::SUCCESS;
+    }
+}
